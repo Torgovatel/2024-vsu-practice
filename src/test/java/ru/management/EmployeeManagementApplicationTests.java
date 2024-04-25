436032cb0afdf86ea17457fe8929628cda9d96df
Torgovatel <kazanin.02@mail.ru>
2024-04-25 13:49:27 +0300

    @feature Project restart, initial connection settings in the psql database

---
+++ src/test/java/ru/management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
