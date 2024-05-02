7b54e17ba4b796652f0e334b52502f7b129aa6eb
Torgovatel <kazanin.02@mail.ru>
2024-05-02 21:41:30 +0300

    refactor: Выпиливание нереализованных таблиц
    Из реализации временно удалены заготовки для таблиц связи сотрудника с отделом: Department, EmployeeDepartment

--- src/main/java/ru/management/store/repositories/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.management.store.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.management.store.entities.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/ru/management/store/repositories/EmployeeDepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.management.store.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.management.store.entities.EmployeeDepartment;
-
-public interface EmployeeDepartmentRepository extends JpaRepository<EmployeeDepartment, Long> {
-}
